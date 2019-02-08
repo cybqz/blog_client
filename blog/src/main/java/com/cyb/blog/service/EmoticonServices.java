8df3fdac93f06eeed3d4374047d8cdb31712f4f3
cybqzwork <cybqzwork@163.com>
2019-02-08 20:17:16 +0800

    V1.0

--- blog/src/main/java/com/cyb/blog/service/EmoticonServices.java
+++
@@ -1,9 +0,0 @@
-package com.cyb.blog.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface EmoticonServices {
-
-	public List<Map<String,String[]>> getEmoticonList(String basePath);
-}
